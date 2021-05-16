fa2d796453fb3dcf891293da414f3f157bbe553b
Stepan Snigur <snigur-stepan@mail.ru>
2021-05-16 17:11:29 +0300

    Change CORS

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(cors())
 app.use('/auth', authRouter)
 app.use('/board', boardRouter)
-app.use(cors({ origin: true }))
 
 const startServer = async () => {
   try {
