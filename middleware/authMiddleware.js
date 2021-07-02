ba08093a15b34db8e36aaf8ee474d8b427281f98
Stepan Snigur <snigur-stepan@mail.ru>
2021-07-02 16:42:19 +0300

    Change checking role middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,13 +5,13 @@ module.exports = function(req, res, next) {
 
   try {
     const token = req.headers.authorization.split(' ')[1]
-    if (!token) return res.status(403).json({ message: 'Пользователь не авторизован' })
+    if (!token) return res.status(401).json({ message: 'Пользователь не авторизован' })
 
     const data = jwt.verify(token, process.env.SECRET_JWT_KEY)
     req.user = data
     next()
   } catch (e) {
     console.error(e)
-    return res.status(403).json({ message: 'Пользователь не авторизован' })
+    return res.status(401).json({ message: 'Пользователь не авторизован' })
   }
 }
--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,20 +1,24 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
-module.exports = (roles) => function(req, res, next) {
+module.exports = (roles) => async function(req, res, next) {
   if (req.method === 'OPTIONS') next()
 
   try {
     const token = req.headers.authorization.split(' ')[1]
-    if (!token) return res.status(403).json({ message: 'Пользователь не авторизован' })
+    if (!token) return res.status(401).json({ message: 'Пользователь не авторизован' })
 
-    const { userBoards } = jwt.verify(token, process.env.SECRET_JWT_KEY)
+    const { id } = jwt.verify(token, process.env.SECRET_JWT_KEY)
     const { boardId } = req.body
-    const userRoleInBoard = userBoards.find(board => board.boardId === boardId).role
+
+    const user = await User.findById(id.toString())
+    const currentBoard = user.registeredInBoards.find(board => board.boardId === boardId)
+    const userRoleInBoard = currentBoard.role
     const hasAccess = roles.includes(userRoleInBoard)
     if (!hasAccess) return res.status(403).json({ message: 'У вас нет доступа' })
     next()
   } catch (e) {
     console.error(e)
-    return res.status(403).json({ message: 'Пользователь не авторизован' })
+    return res.status(401).json({ message: 'Пользователь не авторизован' })
   }
 }
